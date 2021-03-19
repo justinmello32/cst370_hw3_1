aa8ec473ffc90214125252dae2ee0e73c4f611bb
Justin Mello <mello23@llnl.gov>
2021-03-19 08:05:20 -0700

    Intial cleanup of user input complete.

--- hw3_1.cpp
+++ hw3_1.cpp
@@ -3,20 +3,33 @@
 #include <string>
 
 using namespace std;
+
+
+// Function to remove all spaces from a given string
+string removeSpaces(string str)
+{
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
+
 int main() {
 
     string initialInput;
     string validString;
 
     //Gather user input
-    cin >> initialInput;
+    getline(std::cin, initialInput);
+
+    //First Remove spaces from string
+    initialInput = removeSpaces(initialInput);
 
     //Strip tags
-
+    initialInput.erase(remove_if(initialInput.begin(), initialInput.end(), [](char c) { return ispunct(c); } ), initialInput.end());
 
     //Transform string to uppercase
     transform(initialInput.begin(), initialInput.end(), initialInput.begin(), ::toupper);
 
+    //Check if palindrome
 
 
     cout << initialInput << endl;
